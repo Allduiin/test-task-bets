6c973f08dc4f557c4eb46d36e7142b89ffe24eae
Allduiin <vovapro01@gmail.com>
2021-07-27 14:54:24 +0300

    Added working UserServiceImpl.getDices method added private modifiers to models

--- src/main/java/test/task/bets/service/impl/UserServiceImpl.java
+++ src/main/java/test/task/bets/service/impl/UserServiceImpl.java
@@ -1,5 +1,11 @@
 package test.task.bets.service.impl;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import test.task.bets.model.Bet;
@@ -13,6 +19,10 @@ public class UserServiceImpl implements UserService {
     private static final Short MAX_WIN_DICE = 1;
     private static final Float MAX_WIN = 30f;
     private static final Float MIDDLE_WIN = 7f;
+    private static final String GET_DICES_URL =
+            "https://www.random.org/integers/?num=2&min=1&max=6&col=2&base=10&format=plain";
+    private static final Integer DICES1_PLACE = 0;
+    private static final Integer DICES2_PLACE = 2;
     private final UserRepository userRepository;
 
     @Override
@@ -24,17 +34,31 @@ public class UserServiceImpl implements UserService {
     public Bet bet(Long userId, Float stake) {
         Bet bet = new Bet();
         bet.setStake(stake);
-        Short[] dices = getDices();
-        assert dices != null;
+        Short[] dices;
+        try {
+            dices = getDices();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException("Problem with getting dices", e);
+        }
         bet.setDice1(dices[0]);
         bet.setDice2(dices[1]);
         Float winnings = (dices[0].equals(dices[1])) ? (stake
-                * ((dices[0].equals(MAX_WIN_DICE)) ? MAX_WIN: MIDDLE_WIN)): -stake;
+                * ((dices[0].equals(MAX_WIN_DICE)) ? MAX_WIN : MIDDLE_WIN))
+                : -stake;
         bet.setWinnings(winnings);
         return bet;
     }
 
-    private Short[] getDices() {
-        return null;
+    private Short[] getDices() throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(GET_DICES_URL))
+                .GET()
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        String dicesString = response.body();
+        return new Short[]{
+                (short) (dicesString.charAt(DICES1_PLACE) - 48),
+                (short) (dicesString.charAt(DICES2_PLACE) - 48)};
     }
 }
