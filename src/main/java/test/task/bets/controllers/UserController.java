84b18988303618f93a8bc48e31470740838dd758
Allduiin <vovapro01@gmail.com>
2021-07-27 15:35:34 +0300

    Checkstyle

--- src/main/java/test/task/bets/controllers/UserController.java
+++ src/main/java/test/task/bets/controllers/UserController.java
@@ -1,6 +1,5 @@
 package test.task.bets.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import test.task.bets.mapper.UserMapper;
 import test.task.bets.model.Bet;
-import test.task.bets.model.User;
 import test.task.bets.model.dto.UserDto;
 import test.task.bets.service.UserService;
 
@@ -19,9 +17,9 @@ import test.task.bets.service.UserService;
 @RestController
 @RequestMapping("snakeeyes")
 public class UserController {
+    private static final List<Float> STAKES = List.of(1f, 2f, 10f);
     private final UserService userService;
     private final UserMapper userMapper;
-    private static final List<Float> STAKES = List.of(1f, 2f, 10f);
 
     @PostMapping("/add")
     public void add(UserDto userDto) {
@@ -30,9 +28,9 @@ public class UserController {
 
     @GetMapping("play/{id}")
     public Bet play(@PathVariable Long id, @RequestParam Float stake) {
-        if(!STAKES.contains(stake)) {
+        if (!STAKES.contains(stake)) {
             throw new RuntimeException("Unexcited stake");
         }
-        return userService.bet(id,stake);
+        return userService.bet(id, stake);
     }
 }
